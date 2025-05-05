1e13efb8c0addb0961f0e6289f0dd7c0e38c56a1
Hrechko Oleg <ipz233_goo@student.ztu.edu.ua>
2025-05-06 00:53:18 +0300

    патерн Ітератор

---
+++ Lab_3/Features/Iterator/LightNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace LightHTML.Features.Iterator
+{
+    public class LightNode
+    {
+        public string TagName { get; set; }
+        public List<LightNode> Children { get; set; } = new List<LightNode>();
+
+        public void AddChild(LightNode child) => Children.Add(child);
+
+        public virtual void Render()
+        {
+            Console.WriteLine($"Rendering: {TagName}");
+        }
+    }
+}
